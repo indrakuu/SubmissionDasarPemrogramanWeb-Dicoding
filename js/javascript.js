413819fe3063da95a49c9890b6d38402069de86b
Indra Kurniawan <67799408+indrakuu@users.noreply.github.com>
2020-08-03 16:26:26 +0700

    submission dicoding web dasar
    sudah selesai submission dicoding web dasar

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+alert("Halo Selamat Datang")
+alert("Sebelum masuk, Yuk perkenalan diri dulu :)")
+
+const namaAnda = prompt("Siapa namamu?")
+
+alert("Senang bisa berkenalan denganmu ^-^ " + namaAnda)
